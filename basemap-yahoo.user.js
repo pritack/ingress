d3362ac5ee50d76d3349f1ab0b8a4e3889c52031
pritack <pri.tack4@gmail.com>
2018-05-17 17:22:29 +0900

    implement Yahoo map layer by using static API

---
+++ basemap-yahoo.user.js
@@ -0,0 +1,84 @@
+// ==UserScript==
+// @id             iitc-plugin-basemap-yahoo
+// @name           IITC plugin: Yahoo map
+// @category       Map Tiles
+// @version        0.0.1.@@DATETIMEVERSION@@
+// @namespace      https://github.com/pritack/ingress
+// @updateURL      @@UPDATEURL@@
+// @downloadURL    @@DOWNLOADURL@@
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Add a blank map layer - no roads or other features.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
+
+//@@PLUGINSTART@@
+
+// PLUGIN START ////////////////////////////////////////////////////////
+
+
+L.YahooLayer = L.TileLayer.extend({
+
+    getTileUrl: function (tilePoint) {
+        //lat
+        var coordy = (tilePoint.y / Math.pow(2, tilePoint.z)) * 2 * Math.PI - Math.PI;
+        var _lat1 = 2 * Math.atan(Math.pow(Math.E, -coordy)) * 180 / Math.PI - 90;
+        var coordy2 = ((tilePoint.y + 1) / Math.pow(2, tilePoint.z)) * 2 * Math.PI - Math.PI;
+        var _lat2 = 2 * Math.atan(Math.pow(Math.E, -coordy2)) * 180 / Math.PI - 90;
+        var _lat = _lat1 + (_lat2 - _lat1) / 2;
+
+        //lng
+        var _lng1 = (tilePoint.x / Math.pow(2, tilePoint.z)) * 360 - 180;
+        var _lng2 = ((tilePoint.x + 1) / Math.pow(2, tilePoint.z)) * 360 - 180;
+        var _lng = _lng1 + (_lng2 - _lng1) / 2;
+
+        //zoom
+        var _z = tilePoint.z + 1;
+
+        return L.Util.template(this._url, L.extend({
+            z: _z,
+            x: _lat,
+            y: _lng
+        }, this.options));
+    }
+});
+
+function wrapper(plugin_info) {
+    if (typeof window.plugin !== 'function') window.plugin = function () { };
+
+    plugin_info.buildName = 'yahoo-japan-map';
+    plugin_info.dateTimeVersion = '201805171800';
+    plugin_info.pluginId = 'yahoo-japan-map';
+
+    var setup = function () {
+        var yahooApiKey = 'dj0zaiZpPVVNaHJ0MElNSVJ3dSZzPWNvbnN1bWVyc2VjcmV0Jng9YmQ-';
+        var yahooRainOpt = { attribution: 'Yahoo Rain Map', maxNativeZoom: 18, maxZoom: 21 };
+        var yahooRain = new L.YahooLayer('https://map.yahooapis.jp/map/V1/static?appid=' + yahooApiKey + '&lat={x}&lon={y}&z={z}&width=256&height=256&scalebar=off&overlay=type:rainfall|datelabel:off', yahooRainOpt);
+        layerChooser.addBaseLayer(yahooRain, 'Yahoo Rainfall Map');
+    };
+
+    setup.info = plugin_info; //add the script info data to the function as a property
+    if (!window.bootPlugins) window.bootPlugins = [];
+    window.bootPlugins.push(setup);
+
+    // if IITC has already booted, immediately run the 'setup' function
+    if (window.iitcLoaded && typeof setup === 'function') setup();
+
+};
+
+// inject code into site context
+var script = document.createElement('script');
+var info = {};
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(info) + ');'));
+(document.body || document.head || document.documentElement).appendChild(script);
+
+// PLUGIN END //////////////////////////////////////////////////////////
+
+//@@PLUGINEND@@
